57a5ba0daaa12f2f8bbd4d284d271353910ab6be
Nathan Felipi Almeida <nathanfelipi2006@gmail.com>
2024-11-27 21:21:31 -0300

    refatora models

--- src/main/java/model/Hotel.java
+++ src/main/java/model/Hotel.java
@@ -1,4 +1,29 @@
 package model;
 
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
 public class Hotel {
+    private List<Quarto> quartos;
+
+    public Hotel() {
+        this.quartos = new ArrayList<>();
+        inicializarQuartos();
+    }
+
+    private void inicializarQuartos() {
+        for (int i = 1; i <= 9; i++) {
+            quartos.add(new Quarto(i));
+        }
+    }
+
+    public Quarto buscarQuarto(int numero) {
+        return quartos.stream()
+                .filter(quarto -> quarto.getNumero() == numero)
+                .findFirst()
+                .orElse(null);
+    }
 }
--- src/main/java/model/Quarto.java
+++ src/main/java/model/Quarto.java
@@ -1,4 +1,20 @@
 package model;
 
+import lombok.Data;
+import interfaces.EstadoQuarto;
+import interfaces.impls.EstadoDisponivel;
+
+@Data
 public class Quarto {
+    private int numero;
+    private EstadoQuarto estado;
+
+    public Quarto(int numero) {
+        this.numero = numero;
+        this.estado = new EstadoDisponivel(); // Começa como disponível
+    }
+
+    public String getEstado() {
+        return estado.getEstado();
+    }
 }
